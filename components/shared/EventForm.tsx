6b297a4de4aca6945ff7265cc13a2efc911ddd1d
Ansh Vaswani <anshvaswani73@gmail.com>
2024-01-16 16:25:51 +0530

    added edit option to events

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -31,10 +31,10 @@ import { createEvent, updateEvent } from "@/lib/actions/event.actions";
 import { IEvent } from "@/lib/database/models/event.model";
 
 type EventFormProps = {
-  userId: string;
-  type: "Create" | "Update";
-  event?: IEvent;
-  eventId?: string;
+  userId: string,
+  type: "Create" | "Update",
+  event?: IEvent,
+  eventId?: string,
 };
 
 const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
